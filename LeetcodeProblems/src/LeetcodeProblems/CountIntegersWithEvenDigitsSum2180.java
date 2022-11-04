a12421f07e87d2b8e1d1c262dac99636d75b0877
jegan <jegan@172.17.48.169>
2022-11-04 19:03:57 +0530

    Number Changes

--- LeetcodeProblems/src/LeetcodeProblems/CountIntegersWithEvenDigitsSum2180.java
+++ LeetcodeProblems/src/LeetcodeProblems/CountIntegersWithEvenDigitsSum2180.java
@@ -3,7 +3,7 @@ package LeetcodeProblems;
 public class CountIntegersWithEvenDigitsSum2180 {
 	public static void main(String[] args) {
 		int sum=0,rem=0,count=0;
-		int num=4;
+		int num=1;
 		for(int index=1;index<=num;index++) {
 			int temp=index;
 			if(temp>=10) {
